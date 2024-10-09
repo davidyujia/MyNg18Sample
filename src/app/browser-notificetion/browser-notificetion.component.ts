613f59691415be5fe0b91c7c9425697a7e89fdd8
davidyujia <davidyujia@gmail.com>
2024-10-09 14:36:11 +0800

    add browser notification code

---
+++ src/app/browser-notificetion/browser-notificetion.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browser-notificetion',
+  standalone: true,
+  imports: [],
+  templateUrl: './browser-notificetion.component.html',
+  styleUrl: './browser-notificetion.component.scss'
+})
+export class BrowserNotificetionComponent {
+  ngAfterContentInit() {
+
+    if (!('Notification' in window)) {
+      console.error('This browser does not support notification');
+    } else {
+      console.log(`Notification permission: ${Notification.permission}`);
+      Notification.requestPermission().then((result) => {
+        if (result == 'granted') {
+          let notify = new Notification("New Notification", { body: 'message content', tag: 'Notification', icon: '/favicon.ico' });
+          notify.onclick = (e) => {
+            e.preventDefault(); // prevent the browser from focusing the Notification's tab
+
+            notify.close();
+          }
+        }
+      });
+    }
+  }
+}
